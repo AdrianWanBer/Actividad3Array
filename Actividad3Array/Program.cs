7061cbc4b1fa169706122fac8bbab805613ab481
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2023-12-13 09:46:18 +0100

    Agregar archivos de proyecto.

---
+++ Actividad3Array/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Atividad3Array
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            int[,] numeros = new int[10,10];
+
+            int contador = 1;
+
+            //Rellenar los huecos
+            for (int filas = 0; filas < 10; filas++)
+            {
+                for (int columnas= 0 ; columnas < 10; columnas++)
+                {
+                    numeros[filas,columnas] = contador++;
+                    ;
+                }
+            }
+            //Imprimir los numeros
+            for (int filas = 9; filas >= 0; filas--)
+            {
+                for (int columnas = 9; columnas >= 0; columnas--)
+                {
+                    Console.Write(numeros[filas, columnas]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine(" ");
+            //Imprimir numeros de forma ascendente
+            for (int filas = 0; filas < 10; filas++)
+            {
+                for (int columnas = 0; columnas < 10; columnas++)
+                {
+                    Console.WriteLine(numeros[filas,columnas]);
+                }
+            }
+            
+        }
+    }
+}

cb67cbfb81dbf0d7e74286c1c249fb4bcb48c59a
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2023-12-13 09:49:10 +0100

    a

--- Actividad3Array/Program.cs
+++ Actividad3Array/Program.cs
@@ -9,18 +9,18 @@
             int contador = 1;
 
             //Rellenar los huecos
-            for (int filas = 0; filas < 10; filas++)
+            for (int filas = 9; filas >= 0; filas--)
             {
-                for (int columnas= 0 ; columnas < 10; columnas++)
+                for (int columnas = 9 ; columnas >= 0; columnas--)
                 {
                     numeros[filas,columnas] = contador++;
                     ;
                 }
             }
             //Imprimir los numeros
-            for (int filas = 9; filas >= 0; filas--)
+            for (int filas = 0; filas < 10; filas++)
             {
-                for (int columnas = 9; columnas >= 0; columnas--)
+                for (int columnas = 0; columnas < 10; columnas++)
                 {
                     Console.Write(numeros[filas, columnas]);
                     Console.Write(" ");
@@ -29,9 +29,9 @@
             }
             Console.WriteLine(" ");
             //Imprimir numeros de forma ascendente
-            for (int filas = 0; filas < 10; filas++)
+            for (int filas = 9; filas >= 0; filas--)
             {
-                for (int columnas = 0; columnas < 10; columnas++)
+                for (int columnas = 9; columnas >= 0; columnas--)
                 {
                     Console.WriteLine(numeros[filas,columnas]);
                 }
